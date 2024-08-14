76d0b14b19df2b97e23bd57b2f18093d97cbc428
priyamandyal <priya.mandyal98@gmail.com>
2024-08-14 11:13:51 -0300

    test

--- get-url-cloud-formation.sh
+++ get-url-cloud-formation.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+REGION="us-east-1"
 
 if [ -z "$STACK_NAME" ]; then
   echo "STACK_NAME environment variable is not set."
   exit 1
 fi
 
-SUBSCRIBE_API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='SubscribeApiGatewayInvokeUrl'].OutputValue" --output text)
-UPLOAD_IMAGE_API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='UploadImageApiGatewayInvokeUrl'].OutputValue" --output text)
+SUBSCRIBE_API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs[?OutputKey=='SubscribeApiGatewayInvokeUrl'].OutputValue" --output text)
+UPLOAD_IMAGE_API_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION --query "Stacks[0].Outputs[?OutputKey=='UploadImageApiGatewayInvokeUrl'].OutputValue" --output text)
 
 echo "VITE_SUBSCRIBE_API_URL=${SUBSCRIBE_API_URL}" > .env
 echo "VITE_UPLOAD_IMAGE_API_URL=${UPLOAD_IMAGE_API_URL}" >> .env
