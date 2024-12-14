72ab191f15919a11e6a28c6e8bfbf235e1a6e519
priyamandyal <priya.mandyal98@gmail.com>
2024-12-14 18:41:02 -0400

    Updated urls

--- get-url-cloud-formation.sh
+++ get-url-cloud-formation.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 REGION="us-east-1"
-STACK_NAME="test"  # Replace this with your stack name or pass it as an environment variable
+STACK_NAME="test" 
 
-# Fetch the Subscribe API URL
 SUBSCRIBE_API_URL=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --region $REGION --query "Stacks[0].Outputs[?OutputKey=='SubscribeApiGatewayInvokeUrl'].OutputValue" --output text)
 
-# Fetch the Upload Image API URL
 UPLOAD_IMAGE_API_URL=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --region $REGION --query "Stacks[0].Outputs[?OutputKey=='UploadImageApiGatewayInvokeUrl'].OutputValue" --output text)
 
-# Check if the URLs are not empty
 if [[ -z "$SUBSCRIBE_API_URL" || -z "$UPLOAD_IMAGE_API_URL" ]]; then
   echo "Error: One or both API URLs could not be fetched."
   exit 1
 fi
 
-# Save the API URLs to the .env file
 echo "VITE_SUBSCRIBE_API_URL=${SUBSCRIBE_API_URL}" > .env
 echo "VITE_UPLOAD_IMAGE_API_URL=${UPLOAD_IMAGE_API_URL}" >> .env
 
